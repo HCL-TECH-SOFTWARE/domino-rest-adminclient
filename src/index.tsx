bad5a79453e7c608de1f2c113d8e234858307120
denise.soriano <denise.soriano@hcl.com>
2025-02-06 01:01:51 +0800

    Import override theme in index file, update used Shoelace version

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,11 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import '@shoelace-style/shoelace/dist/themes/light.css';
+import '../src/styles/shoelace-overrides.css';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
 import { rootReducer } from './store';
 
-setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.1/cdn/');
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.19.1/cdn/');
 
 const store = configureStore({ reducer: rootReducer });
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
