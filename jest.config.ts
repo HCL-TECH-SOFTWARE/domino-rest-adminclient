80d4a42012a39de3cd734f66304dbe2d049b1d20
denise.soriano <denise.soriano@hcl.com>
2025-04-01 02:22:32 +0800

    Remove axios in package.json

--- jest.config.ts
+++ jest.config.ts
@@ -19,7 +19,7 @@ export default {
   testEnvironmentOptions: {
     url: 'http://localhost/admin/ui'
   },
-  transformIgnorePatterns: ['node_modules/(?!axios|lit-element|lit-html|lit|@lit/|@shoelace)'],
+  transformIgnorePatterns: ['node_modules/(?!lit-element|lit-html|lit|@lit/|@shoelace)'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
