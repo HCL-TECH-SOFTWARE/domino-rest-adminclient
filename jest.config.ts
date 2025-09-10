05ed8fdcf3e01df6affe3fab8779a54ceabd507a
denise.soriano <denise.soriano@hcl.com>
2025-09-10 19:45:19 +0800

    Add uuid to transformIgnorePatterns to use CJS modules in dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -19,10 +19,12 @@ export default {
   testEnvironmentOptions: {
     url: 'http://localhost/admin/ui'
   },
-  transformIgnorePatterns: ['node_modules/(?!lit-element|lit-html|lit|@lit/|@shoelace)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!lit-element|lit-html|lit|@lit/|@shoelace|uuid)',
+  ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
   globals: { TextEncoder: TextEncoder, TextDecoder: TextDecoder },
 };
