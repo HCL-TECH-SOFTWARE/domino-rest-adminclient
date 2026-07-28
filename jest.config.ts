fb458149af6536e0e56ab53880ebbf91abdfade4
Ronald Wilkins <ronald.wilkins@hcl.com>
2026-07-28 11:28:04 -0400

    Upgrade dependencies and resolve compatibility issues
    - Updated npm dependencies to latest compatible versions
    - Added @emotion/react and @emotion/styled for Material-UI 7.x compatibility
    - Fixed TextEncoder/TextDecoder polyfills in jest configuration
    - All tests passing (34 tests, 4 test suites)
    - Build successful with optimized output
    - No breaking changes to application functionality

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
+import { TextEncoder, TextDecoder } from 'util';
+
 export default {
   preset: 'ts-jest',
   transform: {
@@ -26,5 +28,7 @@ export default {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
-  globals: { TextEncoder: TextEncoder, TextDecoder: TextDecoder },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testTimeout: 10000,
+  globals: { TextEncoder, TextDecoder },
 };
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// Polyfills for TextEncoder/TextDecoder used by React Router
+import { TextEncoder, TextDecoder } from 'util';
+
+Object.assign(global, {
+  TextEncoder,
+  TextDecoder,
+});
